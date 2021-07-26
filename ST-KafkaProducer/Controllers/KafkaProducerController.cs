5a42d60dad624c0a1710d559ba737d7923e89131
Pashuta97 <37479234+Pashuta97@users.noreply.github.com>
2021-07-26 15:59:14 +0300

    Add files via upload

---
+++ ST-KafkaProducer/Controllers/KafkaProducerController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Kafka.Producer.API.Controllers
+{
+    [Route("api/kafka")]
+    [ApiController]
+    public class KafkaProducerController : ControllerBase
+    {
+        private readonly ProducerConfig config = new ProducerConfig
+        { BootstrapServers = "localhost:9092" };
+
+        private readonly string topic = "lab";
+        [HttpPost]
+        public IActionResult Post([FromQuery] string message)
+        {
+            return Created(string.Empty, SendToKafka(topic, message));
+        }
+        private Object SendToKafka(string topic, string message)
+        {
+            using (var producer =
+                 new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    if (message == "Start")
+                    {
+                        
+                        var rand = new Random();
+                        //for (int i=0; i<10; i++)
+                        while (true)
+                        {
+                            producer.ProduceAsync(topic, new Message<Null, string> { Value = rand.Next(15,30).ToString() })
+                                .GetAwaiter()
+                                .GetResult();
+                            Thread.Sleep(1000);
+                        }
+                    }
+                    return producer.ProduceAsync(topic, new Message<Null, string> { Value = message })
+                        .GetAwaiter()
+                        .GetResult();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Oops, something went wrong: {e}");
+                }
+            }
+            return null;
+                       
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "st_consumer_group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            using (var builder = new ConsumerBuilder<Ignore,
+                string>(conf).Build())
+            {
+                builder.Subscribe(topic);
+                var cancelToken = new CancellationTokenSource();
+                try
+                {
+                    while (true)
+                    {
+                        var consumer = builder.Consume(cancelToken.Token);
+                        Console.WriteLine($"Message: {consumer.Message.Value} received from {consumer.TopicPartitionOffset}");
+                    }
+                }
+                catch (Exception)
+                {
+                    builder.Close();
+                }
+            }
+            return Task.CompletedTask;
+        }
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
